4d1442d51ed1290bba19af9bbc679f9856051c8a
Winnerkorea <68183508+Winnerkorea@users.noreply.github.com>
2022-10-18 21:40:46 +0900

    11.1

---
+++ src/client/js/videoPlayer.js
@@ -0,0 +1,12 @@
+const video = document.querySelector("video");
+const play = document.querySelector("play");
+const mute = document.querySelector("mute");
+const time = document.querySelector("time");
+const range = document.querySelector("range");
+
+const handleplay = (e) => {};
+
+const handleMute = (e) => {};
+
+play.addEventListener("click", handleplay);
+mute.addEventListener("click", handleMute);
