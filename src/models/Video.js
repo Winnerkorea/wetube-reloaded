76f641ad28558c53f3ddeaeabfeb9eab0d36a7c0
Winnerkorea2020 <68183508+Winnerkorea@users.noreply.github.com>
2022-10-23 07:02:11 +0900

    16.2 Update

--- src/models/Video.js
+++ src/models/Video.js
@@ -10,6 +10,15 @@ const videoSchema = new mongoose.Schema({
   meta: {
     views: { type: Number, default: 0, required: true },
   },
+  comment: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      trim: true,
+      ref: "Comment",
+      maxLength: 20,
+    },
+  ],
   owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
 });
 
